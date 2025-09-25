afa862f56cd61352b46462b66497b09fa8c5c5b5
Andrew Slappendel <andywarren86@gmail.com>
2025-09-25 17:47:54 +1000

    Add simple form login to web-security

--- web-security/src/main/java/dev/slapps/web_security/WebSecurityApplication.java
+++ web-security/src/main/java/dev/slapps/web_security/WebSecurityApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebSecurityApplication {
 
 	public static void main(String[] args) {
+		System.out.println("WebSecurityApplication.main()");
 		SpringApplication.run(WebSecurityApplication.class, args);
 	}
 
