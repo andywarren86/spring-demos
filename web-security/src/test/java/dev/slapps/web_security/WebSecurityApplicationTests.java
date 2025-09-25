014e108d505ed23b66bd25ffd3677a159301ddb5
Andrew Slappendel <andywarren86@gmail.com>
2025-09-25 16:15:22 +1000

    Add web-security app

---
+++ web-security/src/test/java/dev/slapps/web_security/WebSecurityApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.slapps.web_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("WebSecurityApplicationTests.contextLoads()");
+		
+	}
+
+}
